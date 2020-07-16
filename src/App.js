8558d45822f34dc6935ae8503713e3bece8dd665
Saba <sabaazam306@gmail.com>
2020-07-16 09:17:03 +0500

    Basic App Structure

--- src/App.js
+++ src/App.js
@@ -1,20 +1,92 @@
 import React from 'react';
-// import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link, Outlet, useParams } from 'react-router-dom';
 
-export default function App()
-{
-  return <Router>
-      <Route path="/" element={shoes} />
-  </Router>
+export default function App() {
+  return (
+    <Router>
+
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/launch">Launch</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="launch" element={<Launch />}>
+          <Route path="/" element={<LaunchIndex />} />
+          <Route path=":slug" element={<LaunchShoe />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
-// function Home()
-// {
-//   return <div>
-//     <h1>Welcome Home!</h1>
-//   </div>
-// }
+function Home() {
+  return (
+    <div>
+      <h1>Welcome Home!</h1>
+    </div>
+  );
+}
+
+function Launch() {
+  return (
+    <div>
+      <h1>Launch</h1>
+
+      <Outlet />
+
+    </div>
+  );
+}
+
+function LaunchIndex() {
+  return (
+    <ul>
+      {Object.entries(shoes).map(([slug, { name, img }]) => (
+      <li key={slug}>
+        <Link to={`/launch/${slug}`} >
+          <h2>{name}</h2>
+          <img src={img} alt={name} />
+        </Link>
+      </li>
+      ))}
+    </ul>
+  );
+}
+
+function LaunchShoe()
+{
+
+  const { slug } = useParams();
+  const shoe = shoes[slug];
+
+  if(!shoe)
+  {
+    return <h2>Not Found</h2>
+  }
+
+  const {name, img} = shoe;
+
+  return (
+    <div>
+      <h2>{name}</h2>
+      <img src={img} alt={name} />
+    </div>
+  );
+}
+
+function NotFound()
+{
+  return (
+    <div>
+      <h1>Not Found!</h1>
+      <p>Sorry your page was not found!</p>
+    </div>
+  );
+}
 
 const shoes = {
   "air-jordan-3-valor-blue":
